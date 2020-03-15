d6d1e0f80263477490f8805ffe61dadd18306dfd
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-03-15 08:59:04 +0700

    Create script.js

---
+++ HTML/caculator/script.js
@@ -0,0 +1,13 @@
+function calculation() {
+    document.calc.output.value = eval(document.calc.output.value);
+    
+}
+
+function erase() {
+	document.calc.output.value = '';
+}
+
+// variable s contains the actual value of the button pressed 
+function entries(s) {
+	document.calc.output.value+=s;
+}
