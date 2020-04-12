701e8876265e7277eb3658ab4dc73275129ad9a8
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-04-12 11:44:56 +0700

    Create ClassToaDo.h

---
+++ OOP/ClassToaDo/ClassToaDo/ClassToaDo.h
@@ -0,0 +1,17 @@
+#pragma once
+class ClassToaDo
+{
+private:
+	int hdo;
+	int tdo;
+public:
+	ClassToaDo();
+	ClassToaDo(const ClassToaDo&);
+	void Nhap();
+	void Xuat();
+	void TinhTien(int x,int y);
+	double Distance(const ClassToaDo&);
+
+
+};
+
