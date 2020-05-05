64ed058a09e441dff198d8a66922c0a3ddd4e56b
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-05-05 11:40:42 +0700

    Create main.cpp

---
+++ OOP.KHCL/19521242-TH4/Date/Date/main.cpp
@@ -0,0 +1,18 @@
+#include"Date.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	Date x, y,z;
+	cin >> x;
+	cout << x<<endl;
+	cin >> y;
+	cout << y << endl;
+	z = x + 20;
+	cout << z<<endl;
+	++x;
+	cout << x << endl;
+	--x;
+	cout << x;
+	
+}
