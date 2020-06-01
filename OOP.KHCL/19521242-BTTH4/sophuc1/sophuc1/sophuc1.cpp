03d9ba3d4fde264d731ad7cb104c8437079bbb44
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-06-01 10:38:07 +0700

    Create sophuc1.cpp

---
+++ OOP.KHCL/19521242-BTTH4/sophuc1/sophuc1/sophuc1.cpp
@@ -0,0 +1,65 @@
+// sophuc1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "sophuc.h"
+using namespace std;
+sophuc::sophuc()
+{
+	thuc = 1;
+	ao = 0;
+}
+// khoi tao so phuc
+istream& operator >> (istream& is, sophuc& ps)
+{
+	cout << " nhap phan thuc";
+	is >> ps.thuc;
+	cout << "nhap phan ao";
+	is >> ps.ao;
+	return is;
+}// phuong thuc nhap
+ostream& operator << (ostream& os, sophuc ps)
+{
+	os << ps.thuc << "+" << ps.ao << "i";
+	return os;
+ }//phuong thuc xuat
+sophuc sophuc::operator+(sophuc a)
+{
+	sophuc b;
+	b.thuc = a.thuc + thuc;
+	b.ao = a.ao + ao;
+	return b;
+}//cong so phuc
+sophuc sophuc::operator-(sophuc a)
+{
+	sophuc b;
+	b.thuc = a.thuc - thuc;
+	b.ao = a.ao - ao;
+	return b;
+}//cong so ao
+sophuc sophuc::operator*(sophuc a)
+{
+	sophuc b;
+	b.thuc = a.thuc * thuc - a.ao * ao;
+	b.ao = thuc * a.ao + ao * a.thuc;
+	return b;
+}// nhan 2 so ao a1*a2-b1*b2+i*(a1*b2+a2*b1)
+sophuc sophuc::operator/(sophuc a)
+{
+	sophuc b;
+	b.thuc = (a.thuc * thuc + a.ao * ao) / (a.thuc * a.thuc + a.ao * a.ao);
+	b.ao = (-thuc * a.ao + ao * a.thuc) / (a.thuc * a.thuc + a.ao * a.ao);
+	return b;
+}
+bool sophuc::operator==(sophuc a)
+{
+	if (thuc == a.thuc && ao == a.ao)
+		return true;
+	return false;
+}//so sanh bang
+bool sophuc::operator!=(sophuc a)
+{
+	if (a.thuc != thuc || a.ao != ao)
+		return true;
+	return false;
+}// kiem tra khac nhau
