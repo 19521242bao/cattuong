0f0868bd0b267c232102322912838ce3f0a6c816
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-06-01 10:39:36 +0700

    Create DaThuc.h

---
+++ OOP.KHCL/19521242-BTTH4/DaThuc/DaThuc/DaThuc.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+class DaThuc
+{
+private:
+	int bac;
+	double* heso;
+public:
+	DaThuc();
+	DaThuc(const DaThuc&);
+	friend istream& operator>>(istream&, DaThuc&);
+	friend ostream& operator<<(ostream&, DaThuc&);
+	bool operator==(const DaThuc&);
+	bool operator>(const DaThuc&);
+	bool operator<(const DaThuc&);
+	DaThuc operator+(const DaThuc&);
+	DaThuc operator-(const DaThuc&);
+	DaThuc operator*(const DaThuc&);
+	DaThuc operator++();//tinh nguyen ham
+	DaThuc operator--();//tinh dao ham
+	
+};
