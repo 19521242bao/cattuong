86e273ae525c884693a87d34fc87771ee499a896
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-06-01 10:38:30 +0700

    Create main.cpp

---
+++ OOP.KHCL/19521242-BTTH4/CExpr/CExpr/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"CExpr.h"
+using namespace std;
+
+int main() {
+	CExpr calculate("2 + 3 * 5 - (3 + (7 + 2) * 3) / 2");
+	cout << "2 + 3 * 5 - (3 + (7 + 2) * 3) / 2 = " << calculate.GetRes();
+	return 0;
+}
