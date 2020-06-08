9c7ded8c71ddfadce6a7708a25505014fef3ac7a
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-06-08 20:20:51 +0700

    Create ConCuu.cpp

---
+++ OOP.KHCL/19521242-TH5/19521242-TH/bai6/ConCuu.cpp
@@ -0,0 +1,22 @@
+﻿#include "ConCuu.h"
+#include<iostream>
+#include<random>
+#include<string>
+#include<time.h>
+using namespace std;
+ConCuu::ConCuu()
+{
+	SetCon(rand() % 10 + 1);
+	SetSua(rand() % 5 + 1);
+	SetTieng(tiengkeu);
+}
+ConCuu::~ConCuu() {}
+void ConCuu::print()
+{
+	cout << "Con Cuu nay ";
+	GiaSuc::Print();
+}
+void ConCuu::Say()
+{
+	cout << tiengkeu << endl;
+}
