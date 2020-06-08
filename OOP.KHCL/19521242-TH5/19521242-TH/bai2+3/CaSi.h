2d518845e9da7220a86b88c7af285e2c547fb472
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-06-08 20:13:53 +0700

    Create CaSi.h

---
+++ OOP.KHCL/19521242-TH5/19521242-TH/bai2+3/CaSi.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"Career.h"
+using namespace std;
+class CaSi :
+	public Career
+{
+private:
+	int sobaihat;
+	char bestsong[50];
+public:
+	void Nhap();
+	void Xuat();
+};
+
