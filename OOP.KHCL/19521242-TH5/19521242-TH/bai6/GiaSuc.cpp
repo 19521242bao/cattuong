a065056557e4eb61b177bbb84f0e3cf30845dbfa
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-06-08 20:21:02 +0700

    Create GiaSuc.cpp

---
+++ OOP.KHCL/19521242-TH5/19521242-TH/bai6/GiaSuc.cpp
@@ -0,0 +1,36 @@
+﻿// GiaSuc.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "GiaSuc.h"
+#include<string>
+using namespace std;
+GiaSuc::GiaSuc()
+{
+	SetCon(0);
+	SetSua(0);
+	SetTieng("");
+}
+GiaSuc::GiaSuc(int a, int b,string c)
+{
+	SetCon(b);
+	SetSua(a);
+	SetTieng(c);
+}
+GiaSuc::~GiaSuc(){}
+void GiaSuc::SetCon(int b)
+{
+	SoCon = b;
+}
+void GiaSuc::SetSua(int a)
+{
+	SoLitSua = a;
+}
+void GiaSuc::SetTieng(string c)
+{
+	tiengkeu = c;
+}
+void GiaSuc::Print()
+{
+	cout << "Co" << GetCon() << " con , co " << GetSua() << " lit sua  và keu" << GetTieng() << "  trong vuon " << endl;
+}
