523c8d630b6e5c48d62f8a2e0eb01ad27add2156
PHAMBAO\ASUS <19521242@gm.uit.edu.vn>
2020-07-04 08:07:59 +0700

    Create ConDe.h

---
+++ OOP.KHCL/19521242-TH6/19521242/bai6/ConDe.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "GiaSuc.h"
+class ConDe :
+	public GiaSuc
+{
+private:
+	const string tiengkeu = "GoatGoatGoat";
+public:
+	ConDe();
+	virtual~ConDe();
+	void print();
+	void Say();
+};
+
