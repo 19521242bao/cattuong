dbcc6da52e763734de9ebfbccf40f46cc5f32d3f
PHAMBAO\ASUS <19521242@gm.uit.edu.vn>
2020-07-04 08:03:58 +0700

    Create ConCuu.h

---
+++ OOP.KHCL/19521242-TH6/19521242/bai6/ConCuu.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "GiaSuc.h"
+class ConCuu :
+	public GiaSuc
+{
+private:
+	const string tiengkeu = "SheepSheepSheep";
+public:
+	ConCuu();
+	void print();
+	void Say();
+	virtual ~ConCuu();
+};
+
