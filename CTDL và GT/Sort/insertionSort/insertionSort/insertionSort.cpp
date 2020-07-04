94e69b063187c658fed70d436abf734814ed4648
PHAMBAO\ASUS <19521242@gm.uit.edu.vn>
2020-07-04 10:25:12 +0700

    Update insertionSort.cpp

--- CTDL và GT/Sort/insertionSort/insertionSort/insertionSort.cpp
+++ CTDL và GT/Sort/insertionSort/insertionSort/insertionSort.cpp
@@ -7,18 +7,23 @@ void printArray(int a[], int n) {
 		cout << a[i] << " ";
 	}
 }
-void insertionSort(int a[], int n) {
-	int index, value;
-	for (int i = 1; i < n; i++)
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
 	{
-		index = i;
-		value = a[i];
-		while (i > 0 && a[index - 1] > value)
+		key = arr[i];
+		j = i - 1;
+
+		/* Move elements of arr[0..i-1], that are
+		greater than key, to one position ahead
+		of their current position */
+		while (j >= 0 && arr[j] < key)
 		{
-			a[index] = a[index - 1];
-			index--;
+			arr[j + 1] = arr[j];
+			j = j - 1;
 		}
-		a[index] = value;
+		arr[j + 1] = key;
 	}
 }
 int a[100001];
