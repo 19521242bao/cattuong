9973d2afa7f4ee9c6749737b830a2a96f10f7df7
PHAMBAO\ASUS <19521242@gm.uit.edu.vn>
2020-07-04 08:07:34 +0700

    Create ToaDo.h

---
+++ OOP.KHCL/19521242-TH6/19521242/bai2+3+4+5/ToaDo.h
@@ -0,0 +1,26 @@
+#pragma once
+class ToaDo
+{
+private:
+	float hdo;
+	float tdo;
+public:
+	ToaDo();
+	ToaDo(const ToaDo&);
+	ToaDo(int x, int y);
+	void Nhap();
+	void Xuat();
+	int getHD();
+	int getTD();
+	double getCanh(ToaDo a);
+	void setToaDo(int x, int y);
+	void TinhTien(int x, int y);
+	double Distance(const ToaDo&);
+	void Quay(float rad);
+	void Size(float k);
+	~ToaDo();
+
+
+};
+
+
